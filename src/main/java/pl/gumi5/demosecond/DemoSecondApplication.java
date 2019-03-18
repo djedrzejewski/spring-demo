f53628656f6e87153eaf38f74c0c773f443e3061
Dawid Jedrzejewski <dawid.jedrzejewski24@gmail.com>
2019-03-18 19:12:26 +0100

    init

---
+++ src/main/java/pl/gumi5/demosecond/DemoSecondApplication.java
@@ -0,0 +1,13 @@
+package pl.gumi5.demosecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecondApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/gumi5/demosecond/HelloWorldEndpoint.java
@@ -0,0 +1,17 @@
+package pl.gumi5.demosecond;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldEndpoint {
+
+    //localhost:8080/hello
+    //HelloWorld
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    String hello(){
+        return "Hello World!";
+    }
+}
